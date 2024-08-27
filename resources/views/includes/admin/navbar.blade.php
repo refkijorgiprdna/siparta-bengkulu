ca65ad5c74b7e4b0c137c4c9963e133175ee847e
Refki Jorgi Pradana <refki895@gmail.com>
2024-08-27 08:44:24 +0700

    avatar

--- resources/views/includes/admin/navbar.blade.php
+++ resources/views/includes/admin/navbar.blade.php
@@ -61,7 +61,7 @@
             <div class="dropdown d-inline-block">
                 <button type="button" class="btn header-item bg-light-subtle border-start border-end" id="page-header-user-dropdown"
                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user" src="{{ url('siparta\assets\images\users\avatar-1.jpg') }}"
+                    <img class="rounded-circle header-profile-user" src="{{ url('siparta\assets\images\users\avatar.png') }}"
                         alt="Header Avatar">
                     <span class="d-none d-xl-inline-block ms-1 fw-medium">
                         {{ Auth::user()->name }}
